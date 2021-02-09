90cdf4187b78d2d1b7dc900b3706f0c3592f3acd
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-09 13:44:37 +0100

    podpunkt A

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
+        List<Square> squares = new ArrayList<>();
+        Random random = new Random();
+
+        for(int i=0; i<100; i++){
+            squares.add(new Square(random.nextInt(50)+1));
+        }
+        squares.forEach(System.out::println);
+
+        // Podpunkt A
+        List<Square> ex1  = squares.stream().filter(square -> square.getArea() > square.getPerimeter()).collect(Collectors.toList());
+        System.out.println("----- Podpunkt A -----");
+        ex1.forEach(System.out::println);
 
     }
 }
