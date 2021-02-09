c06f915837a811b48579404c45f4870aa73d2a1c
Kosaaaaa <43652764+Kosaaaaa@users.noreply.github.com>
2021-02-09 13:34:29 +0100

    Create Square.java

---
+++ src/main/java/Square.java
@@ -0,0 +1,26 @@
+public class Square {
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    public double getArea() {
+        return Math.pow(this.side, 2);
+    }
+
+    public double getPerimeter() {
+        return 4 * this.side;
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                '}';
+    }
+}
